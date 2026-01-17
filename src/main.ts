8637e8bf2ef81b04782e5dc66f3e94ee2f3d853a
Lord-Einstein <darylleinstein@gmail.com>
2026-01-17 17:40:27 +0100

    feat : Add pagination mode on instant search.

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,8 @@ import './styles/style.css'
 import './components/poke-list-card'
 
 import { pokeListApiFetcher, pokeLiteApiFetcher } from './services/details-api'
-import { pokeApiFetcher } from './services/poke-api';
 
-const displayLimit = 6;
+const displayLimit = 12;
 const NOT_FOUND_IMAGE = "https://cdn3d.iconscout.com/3d/premium/thumb/poke-ball-3d-icon-png-download-4198044.png";
 
 type LitePokemon = {
@@ -18,6 +17,7 @@ let onSearchMode = false;
 
 //je fais un pseudo cache pour optimiser la recherche instantanée et je le vide par sécu
 let pokeCache: LitePokemon[] = [];
+let currentSearchResults: LitePokemon[] = [];
 
 const appDiv = document.querySelector<HTMLDivElement>('#app')!;
 
@@ -26,8 +26,7 @@ appDiv.innerHTML = `
     <h1 class="app-title">Pokédex</h1>
 
     <div class="search-container">
-        <input type="text" id="search-input" class="search-input" placeholder="Rechercher un Pokémon (Nom ou ID)...">
-        <button id="btn-search-trigger" class="btn-search">🔍</button>
+        <input type="text" id="search-input" class="search-input" placeholder="Rechercher un Pokémon (par Nom ou ID)...">
     </div>
     
     <div class="pagination-container" id="pagination-controls">
@@ -54,7 +53,6 @@ const pageInput = document.getElementById('page-input') as HTMLInputElement;
 const totalPagesSpan = document.getElementById('total-pages')!;
 const paginationControls = document.getElementById('pagination-controls')!;
 const searchInput = document.getElementById('search-input') as HTMLInputElement;
-const btnSearch = document.getElementById('btn-search-trigger') as HTMLButtonElement;
 
 
 /**
@@ -65,21 +63,28 @@ function renderSkeletons() {
   container.innerHTML = skeletons;
 }
 
-function calculateTotalPages() {
-  return Math.ceil(totalPokemons / displayLimit);
+function calculateTotalPages(totalRecups: number) {
+  return Math.ceil(totalRecups / displayLimit);
 }
 
 function updatePaginationUI() {
 
+  let maxCount = 0;
+
   if(onSearchMode){
-    paginationControls.style.display = 'none';
-    return;
+    maxCount = currentSearchResults.length;
+  } else {
+    maxCount = totalPokemons;
   }
 
-  paginationControls.style.display = 'inline-flex';
+  if (maxCount === 0) {
+    paginationControls.style.display = 'none';
+  } else {
+    paginationControls.style.display = 'inline-flex';
+  }
 
   const currentPage = (currentOffset / displayLimit) + 1;
-  const totalPages = calculateTotalPages();
+  const totalPages = calculateTotalPages(maxCount);
 
   pageInput.value = currentPage.toString();
   pageInput.max = totalPages.toString();
@@ -114,13 +119,6 @@ async function loadPage(offset: number) {
   //mettre à jour le total des eléments poke
   totalPokemons = data.count;
 
-  //les vraies cartes
-  // const cardsHtml = data.results.map(pokemon => {
-  //   const urlParts = pokemon.url.split('/');
-  //   const id = urlParts[urlParts.length - 2];
-  //   return `<pokemon-card pokemon-id="${id}"></pokemon-card>`;
-  // }).join('');
-
   const cardsHtml = data.results.map(pokemon => {
     const id = getIdFromUrl(pokemon.url);
     return `<pokemon-card pokemon-id="${id}"></pokemon-card>`;
@@ -131,22 +129,42 @@ async function loadPage(offset: number) {
   updatePaginationUI();
 }
 
+function displaySearchResultsPage() {
+    const pageItems = currentSearchResults.slice(currentOffset, currentOffset + displayLimit);
+
+    const cardsHtml = pageItems.map(p => {
+        return `<pokemon-card pokemon-id="${getIdFromUrl(p.url)}"></pokemon-card>`;
+    }).join('');
+
+    container.innerHTML = cardsHtml;
+    updatePaginationUI();
+}
+
 async function instantSearchManager() {
   const searchEntry = searchInput.value.trim().toLowerCase();
 
   if(searchEntry === "") {
-    if(onSearchMode) loadPage(currentOffset);
+    if(!onSearchMode) return;
+    
+    onSearchMode = false;
+    currentOffset = 0; //revenir au début de l'affichage
+    loadPage(currentOffset);
     return;
+
   }
 
   onSearchMode = true;
-  updatePaginationUI();
+  currentOffset = 0; //pour revenir à la first page dès qu'on lance une recherche;
+
 
   //Filtre automatique sur mon cache dans filter
   const filter = pokeCache.filter(p => {
     const id = getIdFromUrl(p.url);
     return p.name.includes(searchEntry) || id === searchEntry;
   });
+
+  // Merde fallait mettre à jour ! putaiiiiiiiiiin !!!! Il m'a fait chier celui là!!!!!
+  currentSearchResults = filter;
 
   //Si il le trouve pas..
   if (filter.length === 0) {
@@ -158,18 +176,42 @@ async function instantSearchManager() {
         <p>Aucun Pokémon ne correspond à "<strong>${searchEntry}</strong>"</p>
       </div>
     `;
+    // je cache aussi la pagin
+    updatePaginationUI();
     return;
   }
 
-  const resultsToShow = filter.slice(0, 20); //réduire la table du tableau des fiches
-    
-  const cardsHtml = resultsToShow.map(p => {
-    return `<pokemon-card pokemon-id="${getIdFromUrl(p.url)}"></pokemon-card>`;
-  }).join('');
-
-  container.innerHTML = cardsHtml;
+  displaySearchResultsPage();
 
 }
+
+
+function goToNextPage() {
+  const maxCount = onSearchMode ? currentSearchResults.length : totalPokemons;
+
+  if (currentOffset + displayLimit < maxCount) {
+    currentOffset += displayLimit;
+    
+    if (onSearchMode) {
+      displaySearchResultsPage();
+    } else {
+      loadPage(currentOffset);
+    }
+  }
+}
+
+function goToPreviousPage() {
+  if (currentOffset > 0) {
+    currentOffset -= displayLimit;
+      
+    if (onSearchMode) {
+      displaySearchResultsPage();
+    } else {
+      loadPage(currentOffset);
+    }
+  }
+}
+
 
 /**
  * Changer de page à partir de l'input
@@ -177,15 +219,22 @@ async function instantSearchManager() {
 function handleInputNavigation() {
 
   let page = parseInt(pageInput.value);
-  const maxPages = calculateTotalPages();
+    
+  // ensuite calculer max selon le mode
+  const maxCount = onSearchMode ? currentSearchResults.length : totalPokemons;
+  const maxPages = calculateTotalPages(maxCount);
 
-  if (isNaN(page) || page < 1) page = 1; //controle des entrées
+  if (isNaN(page) || page < 1) page = 1;
   if (page > maxPages) page = maxPages;
 
   currentOffset = (page - 1) * displayLimit;
-  loadPage(currentOffset);
-}
 
+  if (onSearchMode) {
+    displaySearchResultsPage();
+  } else {
+    loadPage(currentOffset);
+  }
+}
 
 (async function initApp() {
   //charger ma page
@@ -193,30 +242,13 @@ function handleInputNavigation() {
   
   // pendant le chargement de base je télécharge la liste complète pour la recherche..mo cache
   const fullList = await pokeLiteApiFetcher();
-  pokeCache = fullList;
+  pokeCache = fullList; //récupérer le tableau en LitePokemon
   console.log("Base de données Pokémon chargée : ", pokeCache.length, "entrées."); //Ligne de debug
 })();
 
 
-btnPrev.addEventListener('click', () => {
-  if (currentOffset > 0) {
-    currentOffset -= displayLimit;
-    loadPage(currentOffset);
-  }
-});
-
-btnNext.addEventListener('click', () => {
-  // s'assurer de ne pas exced le total d'element poke
-  if (currentOffset + displayLimit < totalPokemons) {
-    currentOffset += displayLimit;
-    loadPage(currentOffset);
-  }
-});
+btnPrev.addEventListener('click', goToPreviousPage);
+btnNext.addEventListener('click', goToNextPage);
 
 pageInput.addEventListener('change', handleInputNavigation);
-
 searchInput.addEventListener('input', instantSearchManager);
-
-
-console.log("Programme lancé");
-loadPage(0);
